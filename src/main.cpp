d30efbeb40ebd775aae890ced0ad55c93c0762d4
Spencer <42276949+Spencer-Sawyer@users.noreply.github.com>
2023-04-26 22:15:28 -0700

    Serial MVP

--- src/main.cpp
+++ src/main.cpp
@@ -9,31 +9,33 @@
 #include <Arduino.h>
 uint8_t  RX_PIN = 18;
 uint8_t  TX_PINS[] = {10,9,8, 7};
-#define TX_SIZE 1 << sizeof(TX_PINS)
+#define TX_SIZE (1 << sizeof(TX_PINS))
 
 #define let auto
 #define ms (1000)
 #define s (1000000)
 #define DEBOUNCE_TIME (50*ms)
 #define PIN_CHANGE_STATE_TIME (10) /*microseconds*/
+#define DEMUX_CHANGE_TIME (1)
 #ifdef USE_KEYBOARD
-uint16_t MAP[9] = {KEY_A,KEY_B,KEY_C,
-                   KEY_D,KEY_E,KEY_F,
-                   KEY_G,KEY_H,KEY_I};
+uint16_t MAP[TX_SIZE] = {KEY_A,KEY_B,KEY_C,
+                         KEY_D,KEY_E,KEY_F,
+                         KEY_G,KEY_H,KEY_I};
 #else
-uint8_t MAP[9] = {1,2,3,
-                  4,5,6,
-                  7,8,9};
+uint8_t MAP[TX_SIZE] = {1,2,3,
+                        4,5,6,
+                        7,8,9};
 #endif
-uint32_t debounce_up[9] =  {0,0,0,
-                            0,0,0,
-                            0,0,0};
-uint32_t debounce_down[9] = {0,0,0,
-                             0,0,0,
-                             0,0,0};
-bool key_state[9] = {false, false, false,
-                     false, false, false,
-                     false, false, false};
+
+uint32_t debounce_up[TX_SIZE] =  {0,0,0,
+                                  0,0,0,
+                                  0,0,0};
+uint32_t debounce_down[TX_SIZE] = {0,0,0,
+                                   0,0,0,
+                                   0,0,0};
+bool key_state[TX_SIZE] = {false, false, false,
+                           false, false, false,
+                           false, false, false};
 void setup() {
   // put your setup code here, to run once:
   // set the TX pins to output and RX for input and make sure they are on pulldown mode
@@ -41,7 +43,7 @@ void setup() {
     pinMode(pin, OUTPUT);
   }
     pinMode(RX_PIN, INPUT_PULLDOWN);
-#if defined(USB_SERIAL) || defined(debug_serial)
+#if defined(USB_SERIAL)
 Serial.begin(115000);
 #endif
 #if defined(USE_JOYSTICK)
@@ -80,53 +82,48 @@ void loop() {
 for (let tx_iter = 0;tx_iter<TX_SIZE;tx_iter++){
 
     for(let tx_pin_num=0;tx_pin_num<sizeof(TX_PINS);tx_pin_num++){
-        if(tx_iter % (2<<tx_pin_num)) digitalWriteFast(TX_PINS[tx_pin_num],HIGH);
+        if(bitRead(tx_iter,tx_pin_num) == 1) digitalWriteFast(TX_PINS[tx_pin_num],HIGH);
         else digitalWriteFast(TX_PINS[tx_pin_num],LOW);
 
 
     }
-  //wait until pin is high
-  delayMicroseconds(PIN_CHANGE_STATE_TIME);
-  auto rx_iter = 0;
+  //wait until pins are high and mux is set up
+  delayMicroseconds(PIN_CHANGE_STATE_TIME+DEMUX_CHANGE_TIME);
   {
       auto rx = RX_PIN;
     if (digitalReadFast(rx))
     {
-      if (micros() - debounce_down[rx_iter][tx_iter] > DEBOUNCE_TIME)
+      if (micros() - debounce_down[tx_iter] > DEBOUNCE_TIME)
       {
-        set_key(MAP[rx_iter][tx_iter]);
+        set_key(MAP[tx_iter]);
         
-        if (!key_state[rx_iter][tx_iter])
+        if (!key_state[tx_iter])
          {
-           debounce_up[rx_iter][tx_iter] = micros();
+           debounce_up[tx_iter] = micros();
          }
 
-          key_state[rx_iter][tx_iter] = true;
+          key_state[tx_iter] = true;
       }
       
     }
     else{
-      if (micros() - debounce_up[rx_iter][tx_iter] > DEBOUNCE_TIME)
+      if (micros() - debounce_up[tx_iter] > DEBOUNCE_TIME)
       {
-        reset_key(MAP[rx_iter][tx_iter]);
+        reset_key(MAP[tx_iter]);
          
-        if (key_state[rx_iter][tx_iter])
+        if (key_state[tx_iter])
         {
-          debounce_down[rx_iter][tx_iter] = micros();
+          debounce_down[tx_iter] = micros();
         }
-          key_state[rx_iter][tx_iter] = false;
+          key_state[tx_iter] = false;
         
       }
-      #if defined(debug_serial)
-      Serial.println(debounce_down[rx_iter][tx_iter]);
-      #endif
 
     }
-    rx_iter++;
   }
-  digitalWriteFast(tx,LOW);
   }
 #if defined(USB_SERIAL)
+
 Serial.println();
 delay(1000);
 #endif
