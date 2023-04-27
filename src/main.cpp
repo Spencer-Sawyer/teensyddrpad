36a7415a4e2fe4d6705c25e2c1f9eadb658f704c
Spencer <42276949+Spencer-Sawyer@users.noreply.github.com>
2023-04-26 18:23:25 -0700

    Refactor to using a muxdemuxer
    Added different build envs for debugging etc.

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,46 @@
 /* 
-READ ME: 
-Be sure to define USB_HID, USB_KEYBOARDONLY, et al. and one of the options defined below
-*/
+ * READ ME:
+ * Be sure to define USB_HID, USB_KEYBOARDONLY, et al. and one of the options defined below
+
+ * #define USE_KEYBOARD
+ * #define USE_SERIAL
+ * #define USE_JOYSTICK
+ */
 #include <Arduino.h>
-uint8_t  RX_PINS[] = {23,21,19};
-uint8_t  TX_PINS[] = {22,20,18};
-/*
-#if (defined(USB_KEYBOARDONLY) || defined(USB_HID) \
- ||  defined(USB_SERIAL_HID) || defined(USB_TOUCHSCREEN) \
- ||  defined(USB_HID_TOUCHSCREEN) || defined(USB_EVERYTHING)) \
- && 0
-#define USE_KEYBOARD
-#elif 0
-#define USE_SERIAL
-#elif 1
-#define USE_JOYSTICK
-#endif*/
-//#define debug_serial
+uint8_t  RX_PIN = 18;
+uint8_t  TX_PINS[] = {10,9,8, 7};
+#define TX_SIZE 1 << sizeof(TX_PINS)
+
+#define let auto
 #define ms (1000)
 #define s (1000000)
 #define DEBOUNCE_TIME (50*ms)
 #define PIN_CHANGE_STATE_TIME (10) /*microseconds*/
 #ifdef USE_KEYBOARD
-uint16_t MAP[3][3] = {{KEY_A,KEY_B,KEY_C},
-                      {KEY_D,KEY_E,KEY_F},
-                      {KEY_G,KEY_H,KEY_I}};
+uint16_t MAP[9] = {KEY_A,KEY_B,KEY_C,
+                   KEY_D,KEY_E,KEY_F,
+                   KEY_G,KEY_H,KEY_I};
 #else
-uint8_t MAP[3][3] = {{4,7,1},
-                     {2,9,5},
-                     {8,6,3}};
+uint8_t MAP[9] = {1,2,3,
+                  4,5,6,
+                  7,8,9};
 #endif
-uint32_t debounce_up[3][3] = {{0,0,0},
-                              {0,0,0},
-                              {0,0,0}};
-uint32_t debounce_down[3][3] = {{0,0,0},
-                                {0,0,0},
-                                {0,0,0}};
-bool keystate[3][3] = {{0,0,0},
-                       {0,0,0},
-                       {0,0,0}};
+uint32_t debounce_up[9] =  {0,0,0,
+                            0,0,0,
+                            0,0,0};
+uint32_t debounce_down[9] = {0,0,0,
+                             0,0,0,
+                             0,0,0};
+bool key_state[9] = {false, false, false,
+                     false, false, false,
+                     false, false, false};
 void setup() {
   // put your setup code here, to run once:
   // set the TX pins to output and RX for input and make sure they are on pulldown mode
   for (auto pin : TX_PINS){
     pinMode(pin, OUTPUT);
   }
-  for (auto pin : RX_PINS){
-    pinMode(pin, INPUT_PULLDOWN);
-  }
+    pinMode(RX_PIN, INPUT_PULLDOWN);
 #if defined(USB_SERIAL) || defined(debug_serial)
 Serial.begin(115000);
 #endif
@@ -83,26 +76,32 @@ Joystick.button(keycode,true);
 }
 void loop() {
   // put your main code here, to run repeatedly:
-auto tx_iter = 0;
-for (auto tx : TX_PINS){
 
-  digitalWriteFast(tx,HIGH);
+for (let tx_iter = 0;tx_iter<TX_SIZE;tx_iter++){
+
+    for(let tx_pin_num=0;tx_pin_num<sizeof(TX_PINS);tx_pin_num++){
+        if(tx_iter % (2<<tx_pin_num)) digitalWriteFast(TX_PINS[tx_pin_num],HIGH);
+        else digitalWriteFast(TX_PINS[tx_pin_num],LOW);
+
+
+    }
   //wait until pin is high
   delayMicroseconds(PIN_CHANGE_STATE_TIME);
   auto rx_iter = 0;
-  for (auto rx : RX_PINS){
+  {
+      auto rx = RX_PIN;
     if (digitalReadFast(rx))
     {
       if (micros() - debounce_down[rx_iter][tx_iter] > DEBOUNCE_TIME)
       {
         set_key(MAP[rx_iter][tx_iter]);
         
-        if (!keystate[rx_iter][tx_iter])
+        if (!key_state[rx_iter][tx_iter])
          {
            debounce_up[rx_iter][tx_iter] = micros();
          }
 
-        keystate[rx_iter][tx_iter] = true;
+          key_state[rx_iter][tx_iter] = true;
       }
       
     }
@@ -111,11 +110,11 @@ for (auto tx : TX_PINS){
       {
         reset_key(MAP[rx_iter][tx_iter]);
          
-        if (keystate[rx_iter][tx_iter]) 
+        if (key_state[rx_iter][tx_iter])
         {
           debounce_down[rx_iter][tx_iter] = micros();
         }
-        keystate[rx_iter][tx_iter] = false;
+          key_state[rx_iter][tx_iter] = false;
         
       }
       #if defined(debug_serial)
@@ -126,7 +125,6 @@ for (auto tx : TX_PINS){
     rx_iter++;
   }
   digitalWriteFast(tx,LOW);
-  tx_iter++;
   }
 #if defined(USB_SERIAL)
 Serial.println();
